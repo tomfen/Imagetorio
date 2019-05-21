f2e014d036265253d614eaac5675fa99b39507a1
tomfen <tomfen999@gmail.com>
2019-05-21 15:19:48 +0200

    improved dithering

--- dither.js
+++ dither.js
@@ -1,6 +1,9 @@
 function dither(pix, width, height, palette) {
     var indexes = []
 
+    var nextRowError = new Array((width + 1) * 4).fill(0);
+    var nextPixError = new Array(3).fill(0);
+
     for (var y = 0; y < height; y += 1) {
         for (var x = 0; x < width; x += 1) {
 
@@ -9,28 +12,22 @@ function dither(pix, width, height, palette) {
 
             [r, g, b] = at(pix, y, x, width);
 
+            r += nextRowError[x * 4 + 0] + nextPixError[0];
+            g += nextRowError[x * 4 + 1] + nextPixError[1];
+            b += nextRowError[x * 4 + 2] + nextPixError[2];
+
             [rc, gc, bc, bi] = closest(r, g, b, palette);
 
             var re, ge, be;
-            re = rc - r;
-            ge = gc - g;
-            be = bc - b;
+            re = r - rc;
+            ge = g - gc;
+            be = b - bc;
 
-            if (x < height - 1) {
-                propagateError(pix, y, x + 1, width, re, ge, be, 7 / 16);
-            }
-
-            if (y < height - 1) {
-                if (x > 0) {
-                    propagateError(pix, y + 1, x - 1, width, re, ge, be, 3 / 16);
-                }
-
-                propagateError(pix, y + 1, x, width, re, ge, be, 5 / 16);
-
-                if (x < height - 1) {
-                    propagateError(pix, y + 1, x + 1, width, re, ge, be, 1 / 16);
-                }
-            }
+            propagateError(nextPixError, 0, re, ge, be, 7 / 16);
+            if (x > 0)
+                propagateError(nextRowError, x * 4 + 0, re, ge, be, 3 / 16);
+            propagateError(nextRowError, x * 4 + 1, re, ge, be, 5 / 16);
+            propagateError(nextRowError, x * 4 + 2, re, ge, be, 1 / 16);
 
             setPix(pix, y, x, width, rc, gc, bc);
             indexes.push(bi)
@@ -86,10 +83,8 @@ function closest(r, g, b, palette) {
     return [rc, gc, bc, ic];
 }
 
-function propagateError(pix, y, x, width, r, g, b, fraction) {
-    index = (y * width + x) * 4;
-
-    pix[index] -= r * fraction;
-    pix[index + 1] -= g * fraction;
-    pix[index + 2] -= b * fraction;
+function propagateError(arr, index, re, ge, be, fraction) {
+    arr[index + 0] = re * fraction;
+    arr[index + 1] = ge * fraction;
+    arr[index + 2] = be * fraction;
 }
