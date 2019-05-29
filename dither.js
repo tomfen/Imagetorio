630a13e3b842c6b5e48d484ee96bd932eb54c65b
tomfen <tomfen999@gmail.com>
2019-05-29 15:10:19 +0200

    fixed dithering

--- dither.js
+++ dither.js
@@ -1,8 +1,45 @@
 function dither(pix, width, height, palette, method) {
     if (method == 'fs')
-        return floydSteiberg(pix, width, height, palette);
+        return floydSteinberg(pix, width, height, palette);
     else if (method == 'closest')
         return quantize(pix, palette);
+}
+
+let a = 2.2;
+let aInv = 1 / a;
+
+function floydSteinberg(pix, width, height, palette) {
+    
+    var diffusionMatrix =
+        [[0., 0., 7 / 16],
+        [3 / 16, 5 / 16, 1 / 16]];
+
+    return errorDiffusion(pix, width, height, palette, diffusionMatrix);
+}
+
+function reshape1darray(arr, d1, d2) {
+    var newArr = [];
+
+    var k = 0;
+
+    for (var i = 0; i < d1; i++) {
+        var row = []
+
+        for (var j = 0; j < d2; j++) {
+
+            var r = arr[k + 0] ** a;
+            var g = arr[k + 1] ** a;
+            var b = arr[k + 2] ** a;
+
+            row.push([r, g, b]);
+
+            k += 4;
+        }
+
+        newArr.push(row);
+    }
+
+    return newArr
 }
 
 function quantize(pix, palette) {
@@ -28,27 +65,32 @@ function quantize(pix, palette) {
     return indexes
 }
 
-function floydSteiberg(pix, width, height, palette) {
-    var indexes = []
+function gammaCorrect(palette) {
+    var corected = [];
+    for (var i = 0; i < palette.length; i++) {
+        corected.push([
+            palette[i][0] ** a,
+            palette[i][1] ** a,
+            palette[i][2] ** a]);
+    }
+    return corected;
+}
 
-    var currRowError = new Array((width + 1) * 4).fill(0);
-    var nextRowError = new Array((width + 1) * 4).fill(0);
-    var nextPixError = new Array(3).fill(0);
+function errorDiffusion(pix, width, height, palette, diffusionMatrix) {
+    var indexes = [];
+    var palette = gammaCorrect(palette);
+
+    var mat = reshape1darray(pix, height, width);
+
+    var diffusionOffset = diffusionMatrix[0].findIndex(val=>val != 0) - 1;
 
     for (var y = 0; y < height; y += 1) {
-        currRowError = nextRowError
-        nextRowError = new Array((width + 1) * 4).fill(0);
-
         for (var x = 0; x < width; x += 1) {
 
             var r, g, b;
             var rc, gc, bc;
 
-            [r, g, b] = at(pix, y, x, width);
-
-            r += nextRowError[x * 4 + 0] + nextPixError[0];
-            g += nextRowError[x * 4 + 1] + nextPixError[1];
-            b += nextRowError[x * 4 + 2] + nextPixError[2];
+            [r, g, b] = mat[y][x];
 
             [rc, gc, bc, bi] = closest(r, g, b, palette);
 
@@ -57,18 +99,20 @@ function floydSteiberg(pix, width, height, palette) {
             ge = g - gc;
             be = b - bc;
 
-            propagateError(nextPixError, 0, re, ge, be, 7 / 16);
-            if (x > 0)
-                propagateError(nextRowError, x * 4 + 0, re, ge, be, 3 / 16);
-            propagateError(nextRowError, x * 4 + 1, re, ge, be, 5 / 16);
-            propagateError(nextRowError, x * 4 + 2, re, ge, be, 1 / 16);
+            for (var ye = 0; ye < diffusionMatrix.length; ye++) {
+                var yImg = y + ye;
+                if (yImg >= height) continue;
 
-            setPix(pix, y, x, width, rc, gc, bc);
+                for (var xe = 0; xe < diffusionMatrix[0].length; xe++) {
+                    var xImg = x + xe - diffusionOffset;
+                    if (xImg < 0 || width <= xImg) continue;
+                    propagateError(mat[yImg][xImg], re, ge, be, diffusionMatrix[ye][xe]);
+                }
+            }
+
+            setPix(pix, y, x, width, rc ** aInv, gc ** aInv, bc ** aInv);
             indexes.push(bi)
         }
-
-        [nextRowError, currRowError] = [currRowError, nextRowError]
-        nextPixError.fill(0)
     }
 
     return indexes
@@ -116,10 +160,10 @@ function closest(r, g, b, palette) {
     return [rc, gc, bc, ic];
 }
 
-function propagateError(arr, index, re, ge, be, fraction) {
-    arr[index + 0] = re * fraction;
-    arr[index + 1] = ge * fraction;
-    arr[index + 2] = be * fraction;
+function propagateError(arr, re, ge, be, fraction) {
+    arr[0] += re * fraction;
+    arr[1] += ge * fraction;
+    arr[2] += be * fraction;
 }
 
 function colorDistance(r1, g1, b1, r2, g2, b2) {
